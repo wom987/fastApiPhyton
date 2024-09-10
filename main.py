eab0e880e35166c005c16b2b48f9a351176429cd
wom987 <45610462+wom987@users.noreply.github.com>
2024-09-10 17:26:37 -0600

    feat: get users, add model and return users

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+from uuid import uuid4
+
 from fastapi import FastAPI
+from typing import List
+from models import User, Gender, Role
 
 app = FastAPI()
-
+db: List[User] = [
+    User(id=uuid4(), first_name="John", last_name="Doe", middle_name="Roland", gender=Gender.male, roles=[Role.user]),
+    User(id=uuid4(), first_name="Carlos", last_name="Sanatana", middle_name="D", gender=Gender.male, roles=[Role.admin]),
+    User(id=uuid4(), first_name="Keren", last_name="Morales", middle_name="S", gender=Gender.female, roles=[Role.student, Role.user]),
+]
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Worldcsssss"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name} from get request"}
+    return {"message": f"Hello {name}"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db
---
+++ models.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from typing import Optional
+from uuid import UUID, uuid4
+
+from pydantic.v1 import BaseModel
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Role(str, Enum):
+        admin = "admin"
+        user = "user"
+        student = "student"
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    last_name: str
+    middle_name: str
+    gender: Gender
+    roles: list[Role]
